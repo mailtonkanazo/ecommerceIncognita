32887ce067a5850a6716ee54527373bef4521cb6
Juan Carlos Triana <juancarlostriana80@gmail.com>
2024-05-14 00:48:33 -0500

    try catch aplication

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,17 +1,26 @@
 import Category from "../models/category.js";
 
 async function categoryList(req, res) {
-  const categoria = await Category.find();
-  res.json(categoria);
+  try {
+    const categoria = await Category.find();
+    res.json(categoria);
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
 }
 
 async function categoryCreate(req, res) {
-  const newCategory = await product.create({
-    name: req.body.name,
-    gender: req.body.gender,
-  });
-  res.json(newCategory);
+  try {
+    const newCategory = await product.create({
+      name: req.body.name,
+      gender: req.body.gender,
+    });
+    res.json(newCategory);
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
 }
+
 export default {
   categoryList,
   categoryCreate,
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ import productsControllers from "./controllers/productsControllers.js";
 import categoryControllers from "./controllers/categoryControllers.js";
 
 import express from "express";
+import Product from "./models/products.js";
 
 const app = express();
 
 app.use(express.json());
-app.get("/api/product");
-app.post("/api/product");
+app.get("/api/product", productsControllers.list);
+app.post("/api/product", productsControllers.create);
+app.get("/api/category", categoryControllers.categoryList);
+app.post("/api/category", categoryControllers.categoryCreate);
 
 app.listen(3000, () => {
   console.log("servidor corriendo en el puerto 3000");
